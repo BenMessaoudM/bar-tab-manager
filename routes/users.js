1b7ba7f127e18693ef860d441e3babca3296d611
Mustapha Ben Messaoud <benmessaoud200@gmail.com>
2025-08-11 20:50:15 +0300

    Switch to bcryptsjs for Render

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require('../models/User');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 /**
  * GET /api/users
